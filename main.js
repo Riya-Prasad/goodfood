d40b6970a1597f7f70fd2c2e35c36ea50ae2b7d1
Riya-Prasad <prasadriya02@gmail.com>
2023-07-25 11:34:49 +0530

    removing locomotive animation and adding gsap animation to every section, adding footer-rights section

--- main.js
+++ main.js
@@ -1,23 +1,131 @@
+var image = document.getElementsByClassName("parallax-img");
+new simpleParallax(image, {
+  scale: 1.3,
+  delay: 0.7,
+  transition: "cubic-bezier(0,0,0,0.4)",
+});
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+// hero-sec animation
+ScrollTrigger.matchMedia({
+  "(min-width:598px)": () => {
+    ScrollTrigger.create({
+      trigger: ".hero",
+      start: "top top",
+      end: "bottom bottom",
+      pin: ".hero .hero-img",
+      scrub: true,
+    });
+  },
+});
+
+// wine-sec animation
+ScrollTrigger.matchMedia({
+  "(min-width:598px)": () => {
+    ScrollTrigger.create({
+      trigger: ".wine",
+      start: "top top",
+      end: "bottom bottom",
+      pin: ".wine .wine-img",
+      scrub: true,
+    });
+  },
+});
+
+// split text animation
+window.addEventListener("load", function () {
+  let revealText = document.querySelectorAll(".reveal-text");
+  let results = Splitting({ target: revealText, by: "lines" });
+
+  results.forEach((splitResult) => {
+    const wrappedLines = splitResult.lines
+      .map(
+        (wordsArr) => `
+        <span class="line"><div class="words">
+          ${wordsArr
+            .map(
+              (word) => `${word.outerHTML}<span class="whitespace"> 
+         </span>`
+            )
+            .join("")}
+        </div></span>`
+      )
+      .join("");
+    splitResult.el.innerHTML = wrappedLines;
+  });
+
+  gsap.registerPlugin(ScrollTrigger);
+  let revealLines = revealText.forEach((element) => {
+    const lines = element.querySelectorAll(".line .words");
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: element,
+        toggleActions: "restart none none reset",
+        start: "top 50%",
+      },
+    });
+    tl.set(revealText, { autoAlpha: 1 });
+    tl.from(lines, 1, {
+      yPercent: 200,
+      ease: Power3.easeOut,
+      stagger: 0.25,
+      delay: 0.2,
+    });
+  });
+});
+
+jQuery(document).ready(function () {
+  let splitLines = jQuery("[data-line]");
+  splitLines.each(function (i, j) {
+    let x = new SplitText(j, {
+      type: j.getAttribute("data-line"),
+      linesClass: "line",
+      wordsClass: "word",
+      charsClass: "char",
+    });
+    let delay = j.getAttribute("data-delay");
+
+    x.chars.forEach((ele, index) => {
+      if (delay) {
+        gsap.to(ele, { "--delay": index + parseInt(delay) + "s" });
+      } else {
+        gsap.to(ele, { "--delay": index + "s" });
+      }
+    });
+  });
+  let splitLines2 = jQuery("[data-line-no-animation]");
+  splitLines2.each(function (i, j) {
+    new SplitText(j, {
+      type: j.getAttribute("data-line-no-animation"),
+      linesClass: "line",
+      wordsClass: "word",
+      charsClass: "char",
+    });
+  });
+
+  let splitLines3 = jQuery("[data-line-within-line]");
+  splitLines3.each(function (i, j) {
+    let x = new SplitText(j, { type: "lines", linesClass: "line-inner" });
+    new SplitText(jQuery(j).find(".line-inner"), {
+      type: "lines",
+      linesClass: "line-mask",
+    });
+    let delay = j.getAttribute("data-delay");
+
+    x.lines.forEach((ele, index) => {
+      if (delay) {
+        let delaytamp = index + parseInt(delay);
+        gsap.to(ele, { "--delay": delaytamp + "s" });
+      } else {
+        gsap.to(ele, { "--delay": index + "s" });
+      }
+    });
+  });
+});
+
 jQuery(window).on("load", function () {
   document.body.classList.add("loaded");
 });
-
-// LOCOMOTIVE SCROLL
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
-  getDirection: true,
-  getSpeed: true,
-  inertia: 1,
-});
-
-// Scroll update all pages
-document.addEventListener("DOMContentLoaded", function () {
-  function ScrollUpdateDelay() {
-    setTimeout(function () {
-      scroll.update();
-    }, 400);
-  }
-  ScrollUpdateDelay();
-});
-
